862c6689ebad6d1f9a9ea2c5c5d23b5afcbcf360
JulienBalestra <julien.balestra@gmail.com>
2018-08-26 23:55:02 +0200

    WIP

--- .ci/e2e.sh
+++ .ci/e2e.sh
@@ -4,43 +4,13 @@ set -exuo pipefail
 
 cd $(dirname $0)/..
 
-kubectl apply -f ./examples/deployment.yaml
+kubectl apply -f .ci/argo/controller/
 
-set +e
-while true
-do
-    kubectl get cm to-sync -n default -o json | jq -re .data.foo && break
-    sleep 5
-    echo "====="
-    kubectl logs -l app=kube-sync -n kube-system
-    echo "====="
-done
+argo version || {
+    curl -Lf https://github.com/argoproj/argo/releases/download/v2.1.1/argo-linux-amd64 -o ${GOPATH}/bin/argo
+    chmod +x ${GOPATH}/bin/argo
+}
 
-kubectl apply -f ./.ci/configmap-v2.yaml
+argo submit .ci/workflow.yaml
+argo logs -w kube-sync -f
 
-while true
-do
-    kubectl get cm to-sync -n default -o json | jq -re .data.bar && break
-    sleep 5
-    echo "====="
-    kubectl logs -l app=kube-sync -n kube-system
-    echo "====="
-done
-
-NS=ns-$(uuidgen)
-kubectl create ns ${NS}
-
-while true
-do
-    kubectl get cm to-sync -n ${NS} -o json | jq -re .data.bar && break
-    sleep 5
-    echo "====="
-    kubectl logs -l app=kube-sync -n kube-system
-    echo "====="
-done
-
-set -e
-for ns in $(kubectl get ns -o json | jq -re .items[].metadata.name)
-do
-    kubectl get cm to-sync -o yaml -n ${ns}
-done
